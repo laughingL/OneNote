afa839290af0a12b2b4d37f6bd9c9ba37cc67904
laughing <qq598278273@163.com>
2018-12-17 20:35:23 +0800

    None

--- app/src/main/java/com/laughing/onenote/AddActivity.java
+++ app/src/main/java/com/laughing/onenote/AddActivity.java
@@ -7,18 +7,27 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 
 public class AddActivity extends AppCompatActivity {
-    private EditText mTitle;
-    private EditText mLocation;
-    private EditText mText;
+    private EditText mTitleEditText;
+    private EditText mLocationEditText;
+    private EditText mTextEditText;
     private RadioGroup mRadioGroup;
     private Button mDateButton;
+    private Button mSubmitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        //
-        mTitle = findViewById(R.id.note_title);
-        mLocation = findViewById(R.id.note_location);
-        mText = findViewById(R.id.note_text);
+        //笔记标题
+        mTitleEditText = findViewById(R.id.note_title);
+        //笔记地点
+        mLocationEditText = findViewById(R.id.note_location);
+        //笔记内容
+        mTextEditText = findViewById(R.id.note_text);
+        //笔记类型
+        mRadioGroup = findViewById(R.id.type_rg);
+        //日期按钮
+        mDateButton = findViewById(R.id.date_button);
+        //提交按钮
+        mSubmitButton = findViewById(R.id.submit_button);
     }
 }
